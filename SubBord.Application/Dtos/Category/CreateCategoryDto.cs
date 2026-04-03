aa7e243f9638d2f7cfb92c34bf5f598aaa440389
Fantomas077 <nheltonn@gmail.com>
2026-04-03 15:51:32 +0200

    Create Dto for Category

---
+++ SubBord.Application/Dtos/Category/CreateCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubBoard.Application.Dtos.Category
+{
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ SubBord.Application/Dtos/Category/UpdateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubBoard.Application.Dtos.Category
+{
+    public class UpdateCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
